2e1b883b8130664286693ea9903d72eaeded68f9
ernieshigh <ernie@ernieshigh.dev>
2020-08-06 10:52:07 +0800

    Color settings

--- high-card/src/block/block.js
+++ high-card/src/block/block.js
@@ -14,11 +14,26 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
 const {
     RichText,
 	MediaUpload,
-    InspectorControls // New component!
+    AlignmentToolbar,
+    BlockControls,
+    BlockAlignmentToolbar,
+    InspectorControls,
+    PanelColorSettings,
+    ColorPalette,
 } = wp.blockEditor;
 
 
-import { Button } from '@wordpress/components';
+const { 
+	Toolbar,
+    Button,
+    Tooltip,
+	CheckboxControl, 
+	SelectControl,
+	PanelBody,
+    PanelRow,
+} = wp.components;
+
+const { Component, Fragment } = wp.element;
 
 
 /**
@@ -66,7 +81,15 @@ registerBlockType( 'high-card/block-high-card', {
 			source: 'children',
 			selector: '.high-card-description',
 		},
+		highBackground: {
+			type: 'string',
+			default: '#f6f6f6' // is optional
+		},
 		
+		fontColor: {
+			type: 'string',
+			default: '#888888' // is optional
+		},
 	},
 	example: {
 		attributes: {
@@ -92,13 +115,27 @@ registerBlockType( 'high-card/block-high-card', {
 	edit: ( props ) => {
 		const {
 			className,
-			attributes: { title, mediaID, mediaURL, description },
+			attributes: { title, mediaID, mediaURL, description, fontColor, highBackground },
 			setAttributes,
 			focus
 		} = props;
+		
+		
+		function onChangeBackgroundColor( newBackground ) {
+			setAttributes( { highBackground: newBackground } );
+		}
+		
+		
+		function onChangeFontColor( newColor) {
+			setAttributes( { fontColor: newColor } );
+		}
+		
+		
 		const onChangeTitle = ( value ) => {
 			setAttributes( { title: value } );
 		};
+	
+
 
 		const onSelectImage = ( media ) => {
 			setAttributes( {
@@ -109,34 +146,57 @@ registerBlockType( 'high-card/block-high-card', {
 		const onChangeDescription = ( value ) => {
 			setAttributes( { description: value } );
 		};
-		return (
 		
-		<InspectorControls>
-			//This text will show when the box is selected
-		</InspectorControls>,
 		
-		<div className={ props.className }>
-			<RichText
-				tagName="h3"
-				placeholder={__('Name of of Featured Product', 'high-card')}
-				value={ title }
-				onChange={ onChangeTitle }
-			/>
+		
+			 
+		return ( 
+		
+			<div className={ props.className } style={{backgroundColor: highBackground}}> 
+				<InspectorControls>
+					<PanelBody>
+						<PanelColorSettings
+							title={ __( 'Color Settings' ) }
+							colorValue={ highBackground }
+							initialOpen={true}
+							colorSettings={ [ {
+								value: highBackground,
+								onChange: onChangeBackgroundColor,
+								label: __( 'Choose a background color' ),
+							},
+							{
+								value: fontColor,
+								onChange: onChangeFontColor,
+								label: __( 'Choose text color' ),
+							}							
+						] }
+						></PanelColorSettings>
+					</PanelBody>
+				</InspectorControls>
+		
+			
+				<RichText
+					tagName="h3"
+					placeholder={__('Name of of Featured Product', 'high-card')}
+					style={{color: fontColor}}
+					value={ title }
+					onChange={ onChangeTitle }
+				/>
 				
-			<div className="high-card-image">
-				<MediaUpload
-					onSelect={ onSelectImage }
-					allowedTypes="image"
-					value={ mediaID }
-					render={ ( { open } ) => (
-						<Button
-							className={
-								mediaID
-									? 'image-button'
-									: 'button button-large'
-							}
-							onClick={ open }
-						>
+				<div className="high-card-image">
+					<MediaUpload
+						onSelect={ onSelectImage }
+						allowedTypes="image"
+						value={ mediaID }
+						render={ ( { open } ) => (
+							<Button
+								className={
+									mediaID
+										? 'image-button'
+										: 'button button-large'
+								}
+								onClick={ open }
+							>
 							{ ! mediaID ? (
 								__( 'Upload Image', 'high-card' )
 							) : (
@@ -148,23 +208,24 @@ registerBlockType( 'high-card/block-high-card', {
 									) }
 								/>
 							) }
-						</Button>
+							</Button>
+						) }
+					/>
+				
+				</div>
+				
+				<RichText
+					tagName="div"
+					multiline="p"
+					className="high-card-description"
+					placeholder={ __(
+						'Descriobe image',
+						'gutenberg-examples'
 					) }
+					style={{color: fontColor}}
+					value={description}
+					onChange={ onChangeDescription}
 				/>
-				
-			</div>
-				
-			<RichText
-				tagName="div"
-				multiline="p"
-				className="high-card-description"
-				placeholder={ __(
-					'Descriobe image',
-					'gutenberg-examples'
-				) }
-				value={description}
-				onChange={ onChangeDescription}
-			/>
 			</div>
 		);
 	},
@@ -183,11 +244,13 @@ registerBlockType( 'high-card/block-high-card', {
 	save: ( props ) => {
 		const {
 			className,
-			attributes: { title, mediaURL, description },
+			attributes: { title, mediaURL, description, fontColor, highBackground},
 		} = props;
 		return (
-			<div className={ className }>
-				<RichText.Content tagName="h3" value={ title } />
+			<div className={ className } style={{backgroundColor: props.attributes.highBackground}}>
+			
+				<RichText.Content tagName="h3" value={ title } 
+					style={{color: fontColor}}/>
 
 				{ mediaURL && (
 					<img
@@ -201,6 +264,7 @@ registerBlockType( 'high-card/block-high-card', {
 					tagName="div"
 					className="high-card-description"
 					value={ description}
+					style={{color: fontColor}}
 				/>
 
 			</div>
